9e4cb5e8cba4efecef1e3c27b2d8f5695a1c1457
codeyStein <san.26.2010@icloud.com>
2022-10-31 09:43:49 -0600

    Better install script

--- fnc.sh
+++ fnc.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 flutter create $1
-cd $1/lib
-rm main.dart
-cd ../
-rmdir lib
+cd $1
+rm -rf lib
+flutter pub add google_fonts
 git clone https://github.com/codeyStein/fns_lib.git
 mv fns_lib lib
 
